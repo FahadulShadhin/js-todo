220a7bd2ac3cd1092837b0b17ecfb2edd3b6a0b4
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-11-06 01:01:43 +0600

    prevent empty form submit in login page

--- js/login.js
+++ js/login.js
@@ -2,14 +2,20 @@ const loginForm = document.querySelector('#login-form');
 
 const login = () => { 
     const name = document.querySelector('#name').value.toLowerCase();
-
     const existingUser = localStorage.getItem(name);
+    const form = document.querySelector('form');
+
+    if(name === "") {
+        alert("Please enter your name first.");
+        form.action = "./index.html";
+        return false;
+    }
+
     if (!existingUser) {
         const data = { 'name': name, 'todolist': [] };
         const jsonData = JSON.stringify(data);
         localStorage.setItem(name, jsonData);
     }
-    else console.log('welcome');
 }
 
 loginForm.addEventListener('submit', () => login())
